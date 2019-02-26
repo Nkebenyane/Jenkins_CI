dbee62cbf96866bea48860921e5680057bde42c2
Maipato Nkebenyane <Maipato.Nkebenyane@americantower.com>
2019-02-26 16:18:06 +0200

    made changes

--- f100.sql
+++ f100.sql
@@ -27,7 +27,7 @@ prompt APPLICATION 100 - Sample Application
 -- Application Export:
 --   Application:     100
 --   Name:            Sample Application
---   Date and Time:   16:17 Tuesday February 26, 2019
+--   Date and Time:   16:18 Tuesday February 26, 2019
 --   Exported By:     LEARNING_SCHEMA
 --   Flashback:       0
 --   Export Type:     Application Export
